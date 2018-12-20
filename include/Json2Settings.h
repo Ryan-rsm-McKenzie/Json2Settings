2a30b2f875bea852e64529d31fb97285a02396ee
SniffleMan <ryan__mckenzie@hotmail.com>
2018-12-19 19:29:51 -0800

    Initial commit

---
+++ include/Json2Settings.h
@@ -0,0 +1,184 @@
+#pragma once
+
+#include <vector>  // vector
+#include <string>  // string
+
+#include "json.hpp"  // json
+
+
+class ISetting
+{
+protected:
+	typedef nlohmann::json json;
+
+public:
+	ISetting() = delete;
+	ISetting(std::string a_key, bool a_consoleOK);
+	virtual ~ISetting();
+
+	virtual void		assign(bool a_val);
+	virtual void		assign(int a_val);
+	virtual void		assign(float a_val);
+	virtual void		assign(const char* a_val);
+	virtual void		assign(std::string a_val);
+	virtual void		assign(json& a_val);
+	virtual void		dump() = 0;
+	virtual std::string	getValueAsString() const = 0;
+	const std::string&	key() const;
+
+protected:
+	std::string	_key;
+};
+
+
+static std::vector<ISetting*>* settings = 0;
+static std::vector<ISetting*>* consoleSettings = 0;
+
+
+class bSetting : public ISetting
+{
+public:
+	bSetting() = delete;
+	bSetting(std::string a_key, bool a_consoleOK, bool a_value);
+	virtual ~bSetting();
+
+	virtual void		assign(bool a_val) override;
+	virtual void		assign(int a_val) override;
+	virtual void		assign(float a_val) override;
+	virtual void		dump() override;
+	virtual std::string	getValueAsString() const override;
+
+	operator bool() const;
+
+protected:
+	bool _value;
+};
+
+
+class iSetting : public ISetting
+{
+public:
+	iSetting() = delete;
+	iSetting(std::string a_key, bool a_consoleOK, SInt32 a_value);
+	virtual ~iSetting();
+
+	virtual void		assign(int a_val) override;
+	virtual void		assign(float a_val) override;
+	virtual void		dump() override;
+	virtual std::string	getValueAsString() const override;
+
+	operator SInt32() const;
+
+protected:
+	SInt32 _value;
+};
+
+
+class fSetting : public ISetting
+{
+public:
+	fSetting() = delete;
+	fSetting(std::string a_key, bool a_consoleOK, float a_value);
+	virtual ~fSetting();
+
+	virtual void		assign(int a_val) override;
+	virtual void		assign(float a_val) override;
+	virtual void		dump() override;
+	virtual std::string	getValueAsString() const override;
+
+	operator float() const;
+
+protected:
+	float _value;
+};
+
+
+class sSetting :
+	public ISetting,
+	public std::string
+{
+public:
+	sSetting() = delete;
+	sSetting(std::string a_key, bool a_consoleOK, std::string a_value);
+	virtual ~sSetting();
+
+	virtual void		assign(std::string a_val) override;
+	virtual void		assign(const char* a_val) override;
+	virtual void		dump() override;
+	virtual std::string	getValueAsString() const override;
+};
+
+
+template <typename T>
+class aSetting :
+	public ISetting,
+	public std::vector<T>
+{
+	virtual ~aSetting()
+	{}
+};
+
+
+template <>
+class aSetting<std::string> :
+	public ISetting,
+	public std::vector<std::string>
+{
+private:
+	using json = nlohmann::json;
+
+public:
+	aSetting() = delete;
+	aSetting(std::string a_key, bool a_consoleOK, std::initializer_list<std::string> a_list = {}) :
+		ISetting(a_key, a_consoleOK),
+		std::vector<std::string>(a_list)
+	{}
+
+	virtual ~aSetting()
+	{}
+
+	virtual void assign(json& a_val) override
+	{
+		clear();
+		for (auto& val : a_val) {
+			emplace_back(val.get<std::string>());
+		}
+	}
+
+	virtual void dump() override
+	{
+		_DMESSAGE("%s:", _key.c_str());
+		for (auto& it = begin(); it != end(); ++it) {
+			_DMESSAGE("\t%s", it->c_str());
+		}
+	}
+
+	virtual std::string	getValueAsString() const override
+	{
+		std::string str = _key + ":";
+		for (auto& it = begin(); it != end(); ++it) {
+			str += "\t" + *it + "\n";
+		}
+		return str;
+	}
+};
+
+
+namespace Json2Settings
+{
+	class Settings
+	{
+	public:
+		Settings() = delete;
+
+		static bool			loadSettings(bool a_dumpParse = false);
+		static ISetting*	set(std::string& a_key, int a_val);
+		static void			dump();
+		static void			setFileName(const char* a_fileName);
+
+		static void			free();
+
+	private:
+		static std::string _fileName;
+	};
+}
---
+++ src/Json2Settings.cpp
@@ -0,0 +1,329 @@
+#include "Json2Settings.h"
+
+#include <fstream>  // ifstream
+#include <exception>  // exception
+#include <string>  // string
+
+#include "json.hpp"  // json
+
+
+ISetting::ISetting(std::string a_key, bool a_consoleOK) :
+	_key(a_key)
+{
+	if (!settings) {
+		settings = new std::vector<ISetting*>;
+	}
+	settings->push_back(this);
+	if (a_consoleOK) {
+		if (!consoleSettings) {
+			consoleSettings = new std::vector<ISetting*>;
+		}
+		consoleSettings->push_back(this);
+	}
+}
+
+
+ISetting::~ISetting()
+{}
+
+
+void ISetting::assign(bool a_val)
+{}
+
+
+void ISetting::assign(int a_val)
+{}
+
+
+void ISetting::assign(float a_val)
+{}
+
+
+void ISetting::assign(const char* a_val)
+{}
+
+
+void ISetting::assign(std::string a_val)
+{}
+
+
+void ISetting::assign(json& a_val)
+{}
+
+
+const std::string& ISetting::key() const
+{
+	return _key;
+}
+
+
+bSetting::bSetting(std::string a_key, bool a_consoleOK, bool a_value) :
+	ISetting(a_key, a_consoleOK),
+	_value(a_value)
+{}
+
+
+bSetting::~bSetting()
+{}
+
+
+void bSetting::assign(bool a_val)
+{
+	_value = a_val;
+}
+
+
+void bSetting::assign(int a_val)
+{
+	_value = a_val ? true : false;
+}
+
+
+void bSetting::assign(float a_val)
+{
+	_value = (int)a_val ? true : false;
+}
+
+
+void bSetting::dump()
+{
+	_DMESSAGE("%s: %s", _key.c_str(), _value ? "True" : "False");
+}
+
+
+std::string bSetting::getValueAsString() const
+{
+	return _value ? "True" : "False";
+}
+
+
+bSetting::operator bool() const
+{
+	return _value;
+}
+
+
+iSetting::iSetting(std::string a_key, bool a_consoleOK, SInt32 a_value) :
+	ISetting(a_key, a_consoleOK),
+	_value(a_value)
+{}
+
+
+iSetting:: ~iSetting()
+{}
+
+
+void iSetting::assign(int a_val)
+{
+	_value = a_val;
+}
+
+
+void iSetting::assign(float a_val)
+{
+	_value = (int)a_val;
+}
+
+
+void iSetting::dump()
+{
+	_DMESSAGE("%s: %i", _key.c_str(), _value);
+}
+
+
+std::string iSetting::getValueAsString() const
+{
+	return std::to_string(_value);
+}
+
+
+iSetting::operator SInt32() const
+{
+	return _value;
+}
+
+
+fSetting::fSetting(std::string a_key, bool a_consoleOK, float a_value) :
+	ISetting(a_key, a_consoleOK),
+	_value(a_value)
+{}
+
+
+fSetting::~fSetting()
+{}
+
+
+void fSetting::assign(int a_val)
+{
+	_value = (float)a_val;
+}
+
+
+void fSetting::assign(float a_val)
+{
+	_value = a_val;
+}
+
+
+void fSetting::dump()
+{
+	_DMESSAGE("%s: %f", _key.c_str(), _value);
+}
+
+
+std::string fSetting::getValueAsString() const
+{
+	return std::to_string(_value);
+}
+
+
+fSetting::operator float() const
+{
+	return _value;
+}
+
+
+sSetting::sSetting(std::string a_key, bool a_consoleOK, std::string a_value) :
+	ISetting(a_key, a_consoleOK),
+	std::string(a_value)
+{}
+
+
+sSetting::~sSetting()
+{}
+
+
+void sSetting::assign(std::string a_val)
+{
+	std::string::operator=(a_val);
+}
+
+
+void sSetting::assign(const char* a_val)
+{
+	std::string::operator=(a_val);
+}
+
+
+void sSetting::dump()
+{
+	_DMESSAGE("%s: %s", _key.c_str(), c_str());
+}
+
+
+std::string sSetting::getValueAsString() const
+{
+	return data();
+}
+
+
+namespace Json2Settings
+{
+	bool Settings::loadSettings(bool a_dumpParse)
+	{
+		using nlohmann::json;
+
+		std::ifstream istream(_fileName.c_str());
+		if (!istream.is_open()) {
+			_ERROR("[ERROR] Failed to open .json file!\n");
+		}
+		json j;
+		try {
+			istream >> j;
+			if (a_dumpParse) {
+				_DMESSAGE("[DEBUG] PARSE DUMP\n%s\n", j.dump(4).c_str());
+			}
+			json::iterator it;
+			for (auto& setting : *settings) {
+				it = j.find(setting->key());
+
+				if (it == j.end()) {
+					_ERROR("[ERROR] Failed to find (%s) within .json!\n", setting->key().c_str());
+					continue;
+				}
+
+				switch (it->type()) {
+				case json::value_t::array:
+				{
+					json jArr = it.value();
+					setting->assign(jArr);
+					break;
+				}
+				case json::value_t::string:
+				{
+					std::string str = it.value();
+					setting->assign(str);
+					break;
+				}
+				case json::value_t::boolean:
+				{
+					bool b = it.value();
+					setting->assign(b);
+					break;
+				}
+				case json::value_t::number_integer:
+				case json::value_t::number_unsigned:
+				{
+					int num = it.value();
+					setting->assign(num);
+					break;
+				}
+				case json::value_t::number_float:
+				{
+					float num = it.value();
+					setting->assign(num);
+					break;
+				}
+				default:
+					_DMESSAGE("[ERROR] Parsed value is of invalid type (%s)!\n", j.type_name());
+				}
+			}
+		} catch (std::exception& e) {
+			_ERROR("[ERROR] Failed to parse .json file!\n");
+			_ERROR(e.what());
+			istream.close();
+			return false;
+		}
+
+		istream.close();
+		return true;
+	}
+
+
+	ISetting* Settings::set(std::string& a_key, int a_val)
+	{
+		for (auto& setting : *consoleSettings) {
+			if (setting->key() == a_key) {
+				setting->assign(a_val);
+				return setting;
+			}
+		}
+		return 0;
+	}
+
+
+	void Settings::dump()
+	{
+		_DMESSAGE("=== SETTINGS DUMP BEGIN ===");
+		for (auto& setting : *settings) {
+			setting->dump();
+		}
+		_DMESSAGE("=== SETTINGS DUMP END ===");
+	}
+
+
+	void Settings::setFileName(const char* a_fileName)
+	{
+		_fileName = a_fileName;
+	}
+
+
+	void Settings::free()
+	{
+		delete settings;
+		delete consoleSettings;
+	}
+
+
+	std::string Settings::_fileName = "";
+}
